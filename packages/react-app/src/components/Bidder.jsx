2f316cef8f1497c6d2899ec64e94ea356abf677d
Dan Finlay <dan@danfinlay.com>
2021-10-10 11:43:12 -0700

    Make bidder stack properly

--- packages/react-app/src/components/Bidder.jsx
+++ packages/react-app/src/components/Bidder.jsx
@@ -1,5 +1,5 @@
 import { Skeleton, Typography } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useThemeSwitcher } from "react-css-theme-switcher";
 import Chart from "react-google-charts";
 
@@ -21,24 +21,25 @@ const { Text } = Typography;
 
 const sumReducer = (previousValue, currentValue) => previousValue + currentValue;
 
+const initialBidders = generateRandomBidders();
+
 export default function MattBidder (props) {
   console.log('rendering mattbidder with props', props);
 
-  const bidders = {};
-  for (var i = 0; i < 30; i++) {
-    const name = Math.random();
-    const rand = Math.round(name * 10);
-    bidders[name] = rand;
-  }
+  const [ bidders, setBidders ] = useState(initialBidders);
+  const [ draftBid, setDraftBid ] = useState(0);
 
   const bidData = generateBidData(bidders);
   let topRev = 0;
   let winningBid = 0;
+  let winnerCount = 0;
   for (let i = 1; i < bidData.length; i++) {
     const totalRev = bidData[i].reduce(sumReducer);
+    console.log(`The sum of ${JSON.stringify(bidData[i])} is ${totalRev}`);
     if (totalRev > topRev) {
       topRev = totalRev;
       winningBid = bidData[i][0];
+      winnerCount = bidData[i].filter(n => n >= winningBid).length;
     };
   }
 
@@ -67,7 +68,35 @@ export default function MattBidder (props) {
           }}
           rootProps={{ 'data-testid': '2' }}
       />
-      <p>The current winning price is ${winningBid}, which will net the artist ${topRev}</p>
+      <p>The current winning price is ${winningBid}, which will produce {winnerCount} editions and net the artist ${topRev}</p>
+      <div className="bidPlacing">
+        <input type="number" min="0" onChange={(event) => setDraftBid(parseFloat(event.target.value))}/>
+        <input
+          type="button"
+          value="Place Bid"
+          onClick={() => {
+            const newBidders = {...bidders}
+            newBidders[`user${Math.random()}`] = draftBid;
+            setBidders(newBidders);
+          }}
+        />
+      </div>
+      <div>
+        <input
+          type="button"
+          value="Reset Bids"
+          onClick={() => {
+            setBidders(initialBidders);            
+          }}
+        />
+        <input
+          type="button"
+          value="Randomize Bids"
+          onClick={() => {
+            setBidders(generateRandomBidders());
+          }}
+        />
+      </div>
     </div>
   );
 }
@@ -83,14 +112,16 @@ function generateBidData (bidders) {
 
   const bidderNames = sortedBidArr.map(b => b.addr);
 
-  const ret = [[...bidderNames]];
+  const ret = [['Price Per Item', ...bidderNames]];
 
-  sortedBidArr.forEach((bidder) => {
+  sortedBidArr.forEach((bidder, i) => {
+
+    // Enter their actual price
     const bid = bidder.bid;
-    const bids = sortedBidArr.map((bidder) => {
+    const bids = [bid, ...sortedBidArr.map((bidder) => {
       if (bidder.bid >= bid) return bid;
       return 0;
-    });
+    })];
     ret.push(bids);
   });
 
@@ -98,4 +129,13 @@ function generateBidData (bidders) {
   return ret;
 }
 
-
+function generateRandomBidders () {
+  const bidders = {};
+  // Initialize sample bidders
+  for (var i = 0; i < 30; i++) {
+    const name = Math.random();
+    const rand = Math.round(name * 10);
+    bidders[name] = rand;
+  }
+  return bidders;
+}
